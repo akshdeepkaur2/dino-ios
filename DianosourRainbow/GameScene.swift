65511cccd9b63a615978a67c9a4084109290972c
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-10-08 01:04:50 -0400

    INITIAL COMMIT

--- DianosourRainbow/GameScene.swift
+++ DianosourRainbow/GameScene.swift
@@ -11,100 +11,127 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var player:SKSpriteNode!
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    var rainbow:SKSpriteNode!
+    var poop:SKSpriteNode!
+    var lives = 3
+    var  livesLabel:SKLabelNode!
+    var score = 0
+    var scoreLabel:SKLabelNode!
+    var playerMovingLeft:Bool = false
+    var playerMovingRight:Bool = false
+    var MouseX:CGFloat = 0.0
+    var MouseY:CGFloat = 0.0
+    var ItemArray:[SKSpriteNode] = []
+    override func didMove(to view: SKView) {
+        self.player = SKSpriteNode(imageNamed: "dino64")
+        self.player.position = CGPoint(x: 300, y: 300)
+        addChild(player)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.livesLabel = SKLabelNode(text: "Lives:\(lives)")
+        self.livesLabel.position = CGPoint(x: 300, y: 600)
+        self.livesLabel.color = UIColor.white
+        self.livesLabel.fontColor = UIColor.white
+        self.livesLabel.fontSize = 35
+        addChild(self.livesLabel)
+        self.scoreLabel = SKLabelNode(text: "Score:\(self.score)")
+        self.scoreLabel.position = CGPoint(x:75, y: 600)
+        self.scoreLabel.fontSize = 35
+        self.scoreLabel.fontColor = UIColor.white
+        addChild(self.scoreLabel)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // get the first "tap" on the screen
+        let locationtouched = touches.first
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if (locationtouched == nil) {
+            // if for some reason the "tap" return as null, then exit
+            return
+        }
+        let mousePosition = locationtouched!.location(in:self)
+        print("mouseX = \(mousePosition.x)")
+        print("mouseY = \(mousePosition.y)")
+        print("-------")
+        
+        self.MouseX = mousePosition.x
+        self.MouseY = mousePosition.y
+        
+        
+        let touchLocation = locationtouched!.location(in: self)
+        
+        print("User tapped screen at: \(touchLocation.x.rounded()),\(touchLocation.y.rounded())")
+    }
+    var candy:[SKSpriteNode] = []////////////////////
+    func spawnCandy(){
+        let candy = SKSpriteNode(imageNamed: "candy64")
+        let randomXPos = CGFloat.random(in:0 ... size.width)
+        let randomYPos = CGFloat.random(in:0 ... size.height)
+        candy.position = CGPoint(x: randomXPos, y: randomYPos)
+        addChild(candy)
+        self.candy.append(candy)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    // just to move player according to mouse click
+    func movePlayer(mouseXPosition:CGFloat,mouseYPosition:CGFloat)  {
+        //1. calculate didtance
+        let a = (self.MouseX - self.player.position.x)
+        let b = (self.MouseY - self.player.position.y)
+        let distance = sqrt((a * a) + (b * b))
+        
+        //2. movement per rate
+        
+        let yn = (b / distance)
+       let xn = (b / distance)
+        //calculate the movement
+        
+        self.player.position.y = self.player.position.y + (yn * 40)
+        self.player.position.x = self.player.position.x + (xn * 40)
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+    var numLoops = 0
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        numLoops = numLoops + 1
+        if(numLoops % 120 == 0){/////////////////////////////////////////////////////////////////////////////
+            
+            
+            self.spawnCandy()
+            
+            
+        
+    
+    // get current y position of player
+    
+            let screenRightSide = size.width
+            
+            // get current x-position of zombie
+            var playerX = self.player.position.x
+            
+            if (self.playerMovingLeft == true) {
+                playerX = self.player.position.x - 10;
+                
+                if (playerX <= 0) {
+                    // bounce off left wall
+                    self.playerMovingRight = false;
+                    self.playerMovingLeft = false;
+                    
+                }
+            }
+            
+            if (self.playerMovingRight == true) {
+                playerX = self.player.position.x + 10;
+                
+                if (playerX >= screenRightSide) {
+                    // bounce off right wall
+                    self.playerMovingLeft = false
+                    self.playerMovingRight = false
+                }
+            }
+        
+    if(MouseY != 0.0 && MouseX != 0.0){
+    self.movePlayer(mouseXPosition: self.MouseX, mouseYPosition: self.MouseY)
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
+}
 }
